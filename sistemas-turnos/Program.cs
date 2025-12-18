d6bfd7acad863f50bc5fd3e4fe8e779f4726ed87
jermiasoliva0805 <jeremiasoliva0805@mi.unc.edu.ar>
2025-12-18 16:35:35 -0300

    cambios

---
+++ sistemas-turnos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemas_turnos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
